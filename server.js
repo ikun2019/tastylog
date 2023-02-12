52333db5947842eb54a9323f0805b21ce6e518c3
ikun <0215.miyatto@gmail.com>
2023-02-12 10:51:06 +0900

    ejsを使用するための初期設定

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const colors = require('colors');
 // * 1データベース設定の読み込み
 const sequelize = require('./util/database');
 
+// * 1ejsの読み込み
+const ejs = require('ejs');
+
 // * pathの読み込み
 const path = require('path');
-
+// * expressの読み込み
 const express = require('express');
 
 const dotenv = require('dotenv');
@@ -13,10 +16,21 @@ dotenv.config();
 
 const app = express();
 
+// * 2ejsをappにマウントする
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 // * 静的ファイルの読み込み
 app.use(express.static(path.join(__dirname, 'public')));
 
-// * 2データベースとアプリを同期
+// * ルーティング
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello World'
+  });
+});
+
+// * 2データベースとappを同期
 sequelize
   .sync()
   .then(result => {
