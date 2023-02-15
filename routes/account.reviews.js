3aa1773bc571d7f1501a1d0e0c86789e468e292f
ikun <0215.miyatto@gmail.com>
2023-02-16 05:56:54 +0900

    口コミ登録のエラー処理

--- routes/account.reviews.js
+++ routes/account.reviews.js
@@ -15,6 +15,20 @@ let createReviewData = function (req) {
   };
 };
 
+let validateReviewData = function (req) {
+  let body = req.body;
+  let isValid = true;
+  let error = {};
+  if (body.visit && !moment(body.visit, DATE_FORMAT).isValid()) {
+    isValid = false;
+    error.visit = '訪問日の日付文字列が不正です';
+  }
+  if (isValid) {
+    return undefined;
+  }
+  return error;
+}
+
 // * GET => /account/reviews/regist/:shopId
 router.get('/regist/:shopId(\\d+)', async (req, res, next) => {
   let shopId = req.params.shopId;
@@ -34,9 +48,19 @@ router.get('/regist/:shopId(\\d+)', async (req, res, next) => {
 
 // * POST => /account/reviews/regist
 router.post('/regist/confirm', (req, res, next) => {
+  let error = validateReviewData(req);
   let review = createReviewData(req);
   console.log('review:' ,review);
   let { shopId, shopName } = req.body;
+
+  if (error) {
+    return res.render('account/reviews/regist-form', {
+      shopId,
+      shopName,
+      review,
+      error
+    });
+  }
 
   res.render('account/reviews/regist-confirm', {
     shopId,
@@ -44,5 +68,27 @@ router.post('/regist/confirm', (req, res, next) => {
     review
   })
 });
+
+// * POST => /account/reviews/regist/:shopId
+router.post('/regist/:shopId(\\d+)', (req, res, next) => {
+  let review = createReviewData(req);
+  let { shopId, shopName } = req.body;
+  res.render('account/reviews/regist-form', {
+    shopId,
+    shopName,
+    review
+  });
+});
+
+// * POST => /account/reviews/regist/execute
+// router.post('/regist/execute', (req, res, next) => {
+//   let review = createReviewData(req);
+//   let { shopId, shopName } = req.body;
+//   res.render('account/reviews/regist-form', {
+//     shopId,
+//     shopName,
+//     review
+//   });
+// });
 
 module.exports = router;
