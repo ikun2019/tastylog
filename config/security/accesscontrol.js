6d0e144ec47d479a8ec1a55d19ffd888b26cd28d
ikun <0215.miyatto@gmail.com>
2023-02-18 19:17:46 +0900

    ログイン機能の実装

--- config/security/accesscontrol.js
+++ config/security/accesscontrol.js
@@ -1,5 +1,48 @@
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+// const sequelize = require('../database');
+const User = require('../../models/User');
+const PRIVILEGE = {
+  NORMAL: 'normal'
+};
+
 let initialize, authenticate, authorize;
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  'local-strategy',
+  new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, async (req, username, password, done) => {
+    let results, user;
+    try {
+      results = await User.findOne({ where: { name: req.body.username } });
+    } catch (err) {
+      return done(err);
+    }
+
+    if (results && password === results.password) {
+      user = {
+        id: results.id,
+        name: results.name,
+        email: results.email,
+        permissions: [PRIVILEGE.NORMAL]
+      };
+      done(null, user);
+    } else {
+      done(null, false, req.flash('message', 'ユーザー名またはパスワードが間違っています'));
+    }
+  })
+);
 
 initialize = function () {
   return [
@@ -14,8 +57,20 @@ initialize = function () {
   ];
 };
 
+authenticate = function () {
+  return passport.authenticate(
+    'local-strategy',
+    {
+      successRedirect: '/account',
+      failureRedirect: '/account/login'
+    }
+  );
+};
+
+
 module.exports = {
   initialize,
   authenticate,
-  authorize
+  authorize,
+  PRIVILEGE
 };
--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const accountReviews = require('./account.reviews');
+const { authenticate, authorize, PRIVILEGE } = require('../config/security/accesscontrol');
 
 router.get('/login', (req, res, next) => {
-  res.render('./account/login');
+  res.render('./account/login', { 'message': req.flash('message') });
 });
+
+router.post('/login', authenticate());
 
 router.use('/reviews', accountReviews);
 
